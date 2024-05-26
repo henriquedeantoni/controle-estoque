72599ea0e9dbbd6b5463304bc084ee7981c6e2dc
Henrique de Antoni <henriqdentoni@gmail.com>
2024-05-26 20:57:07 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import {Router, Response, Request} from "express";
+
+const router = Router();
+router.get("/test", (request: Request, response: Response) => {
+    return response.json( {ok: true})
+})
+
+export {router}
