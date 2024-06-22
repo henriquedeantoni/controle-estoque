630e7a9222bd30e2b8ef5da4733ce6be930a37cb
Henrique de Antoni <henriqdeantoni@gmail.com>
2024-06-22 01:13:50 -0300

    DetailUserService.ts created

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,18 @@
-import {Router, Response, Request} from "express";
+import { Router, Response, Request } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { DetailUserController } from "./controllers/user/DetailUserController";
 
 const router = Router();
 router.get("/test", (request: Request, response: Response) => {
-    return response.json( {ok: true})
+    return response.json({ ok: true })
 })
 
 router.post('/user', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
+router.get('/me', isAuthenticated, new DetailUserController().handle);
 
-export {router}
+export { router };
 
 
