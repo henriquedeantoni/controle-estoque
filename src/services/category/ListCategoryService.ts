03e1902ff733aca8c832d1d5f54d4c5973e91941
Henrique de Antoni <henriqdeantoni@gmail.com>
2024-06-23 12:52:23 -0300

    listCategories controller, service and route created

---
+++ src/services/category/ListCategoryService.ts
@@ -0,0 +1,18 @@
+import prismaClient from "../../prisma";
+
+class ListCategoryService{
+    async execute(){
+        // buscar todas as categorias do nosso banco de dados
+
+        const categories = await prismaClient.category.findMany({
+            select: {
+                id: true,
+                name: true
+            },
+        });
+        return categories;
+    }
+
+}
+
+export {ListCategoryService}
